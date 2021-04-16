35df90f0bb59768c52383cddf3b9462f705458bf
Glenn Salaman <g.salaman@comcast.net>
2021-04-16 17:44:56 -0600

    two fixes

--- button_emulator.py
+++ button_emulator.py
@@ -9,8 +9,9 @@ class ButtonEmulator():
     self.client = mqtt.Client("Button_Emulator")
     # note we don't need "on_message" because we only publish.
 
-    #self.client.connect("mqttbroker")
-    self.client.connect("broker.hivemq.com")
+    self.client.connect("mqttbroker")
+    #self.client.connect("matrix-pi1.local")
+    #self.client.connect("broker.hivemq.com")
 
   def create_widgets(self):
     self.buttons = [[None for _ in range(8)] for _ in range(8)]
@@ -21,7 +22,7 @@ class ButtonEmulator():
                                 text=str(i)+","+str(j), 
                                 command=lambda i=i, j=j:self.button_press(self.buttons[i][j]))
 
-        self.buttons[i][j].grid(row=i, column=j)
+        self.buttons[i][j].grid(row=j, column=i)
 
   def button_press(self, button):
     self.client.publish("jumbotron/button/press", "P"+button["text"])
